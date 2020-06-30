9d37af5893d41a3ddcd8f196591aef3c92866d5e
Lucas Canete <57593487+LucasCanete@users.noreply.github.com>
2020-06-30 13:39:34 +0200

    Add files via upload

---
+++ aufgabe9.cpp
@@ -0,0 +1,108 @@
+/*Written by Lucas Canete am 24.Mai 2020*/
+/*This program checks if a given string is a palindrom (reversed word is the same as original word)
+and returns the reversed string, without any spaces or symbols that do not belong to the alphabet
+*/
+#include<iostream>
+
+
+
+using namespace std;
+
+
+string clean(const std::string& text){
+//Deletes chars that do not contain letters
+
+    string new_text = text;
+
+    int length = text.size();
+
+    for (int i = 0;i<length;i++){
+        //When following chars are found,delete them
+       if(new_text[i] == ' ' || new_text[i] == '-' || new_text[i] == '!' || new_text[i] == '?' || new_text[i] == '.' || new_text[i] == ',')
+            new_text.erase(new_text.begin() + i);
+    }
+return new_text;
+}
+
+void swap(char& a, char& b){
+    //Convert one string to another one by every letter
+    a = b;
+
+}
+
+
+
+
+bool reverse(std::string& text){
+    //Reverse text
+
+    string new_text = text;
+    int length = text.size()-1;
+    int counter = text.size()-1;
+
+    for(int i = 0; i<=length;i++){
+        //Last letter takes value from the last one.
+        text[i] = new_text[counter];
+
+        counter--;
+    }
+
+
+//When new string is the same as the original, then return true
+bool flag = true;
+if (new_text != text){
+
+    flag = false;
+
+}
+return flag;
+
+}
+
+
+
+
+
+int main(){
+
+string text;
+cout<<"Geben Sie eine Zeile ein: ";
+getline(cin,text);
+
+cout<< "Ohne Sonderzeichen "<<clean(text)<<"."<<endl;
+string neu_text = clean(text);
+cout<<"Laenge: "<<neu_text.size()<<endl;
+
+
+for (int i = 0;i<text.size();i++){
+
+
+
+    swap(text[i],neu_text[i]);
+
+}
+for (int i = text.size()-1; i>=0 ;i--){
+
+    if(text[i]=='\0'){
+        text.erase(text.begin() + i);
+    }
+
+}
+
+reverse(text);
+cout<<"Umgekehrte Reihenfolge: "<<text<<endl;
+cout<<"Palindrom? ";
+if (reverse(text))
+    cout<<"Ja.";
+else{
+    cout<<"Nein.";
+}
+
+
+
+
+
+
+
+return 0;
+}
